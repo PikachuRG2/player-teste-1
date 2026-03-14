19c8eb308275614bdcef14d9740d6404992562d5
Rodrigo <rafael2019rg@gmail.com>
2026-03-13 23:43:19 -0300

    Update HLS.js

--- HLS.js
+++ HLS.js
@@ -3,28 +3,28 @@
 "name": "Globo",
 "logo": "http://p6.vc/VCK",
 "group": "Abertos",
-"url": "http://gerenciar.vivatele.com:80/viva_padrao/viva2384/1"
+"url": "https://corsproxy.io/?http://gerenciar.vivatele.com:80/viva_padrao/viva2384/1"
 },
 {
 "name": "SBT",
 "logo": "http://p6.vc/VBS",
 "group": "Abertos",
-"url": "http://gerenciar.vivatele.com:80/viva_padrao/viva2384/2"
+"url": "https://corsproxy.io/?http://gerenciar.vivatele.com:80/viva_padrao/viva2384/2"
 },
 {
 "name": "Band",
 "logo": "http://p6.vc/VVI",
 "group": "Abertos",
-"url": "http://gerenciar.vivatele.com:80/viva_padrao/viva2384/3"
+"url": "https://corsproxy.io/?http://gerenciar.vivatele.com:80/viva_padrao/viva2384/3"
 },
 {
 "name": "Record News",
 "group": "Noticias",
-"url": "http://gerenciar.vivatele.com:80/viva_padrao/viva2384/23"
+"url": "https://corsproxy.io/?http://gerenciar.vivatele.com:80/viva_padrao/viva2384/4"
 },
 {
 "name": "Ra Tim Bum",
 "group": "Infantil",
-"url": "http://gerenciar.vivatele.com:80/viva_padrao/viva2384/15"
+"url": "https://corsproxy.io/?http://gerenciar.vivatele.com:80/viva_padrao/viva2384/5"
 }
 ]
