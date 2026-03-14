3859e8319632ca6753e301d8f7ab4cdc59e38030
Rodrigo <rafael2019rg@gmail.com>
2026-03-14 03:31:14 -0300

    Update player.js

--- player.js
+++ player.js
@@ -1,26 +1,49 @@
-let hls;
+document.addEventListener("DOMContentLoaded", () => {
 
+let hls;
 const proxy = "https://player-teste-1.rafael2019rg.workers.dev/?url=";
 
-function play(url){
+fetch("canais.json")
+.then(r => r.json())
+.then(canais => {
 
-const video = document.getElementById("video");
+const video = document.getElementById("video")
+const lista = document.getElementById("lista")
+
+canais.forEach(c => {
+
+const btn = document.createElement("button")
+btn.innerText = c.nome
+
+btn.onclick = () => {
 
 if(hls){
-hls.destroy();
+hls.destroy()
 }
 
 if(Hls.isSupported()){
 
-hls = new Hls();
+hls = new Hls()
 
-hls.loadSource(proxy + encodeURIComponent(url));
-hls.attachMedia(video);
+hls.loadSource(proxy + encodeURIComponent(c.url))
+hls.attachMedia(video)
+
+hls.on(Hls.Events.MANIFEST_PARSED, function(){
+video.play()
+})
 
 }else{
 
-video.src = proxy + encodeURIComponent(url);
+video.src = proxy + encodeURIComponent(c.url)
 
 }
 
 }
+
+lista.appendChild(btn)
+
+})
+
+})
+
+})
